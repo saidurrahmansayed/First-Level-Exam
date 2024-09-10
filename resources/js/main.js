883faaabcd039b06d8ec756c240e74be6c55ab3f
saidurrahmansayed <mdsaidurrahmansayed194@gmail.com>
2024-09-10 21:51:02 +0600

    Add files via upload

---
+++ resources/js/main.js
@@ -0,0 +1,242 @@
+// button-1
+document.getElementById('buy-now-1').addEventListener('click', function(){
+    // get product quantity form html tag and convert it number
+    const productQuantityField = document.getElementById('product-quantity');
+    const productQuantityText = productQuantityField.innerText; 
+    const newProductQuantity = parseFloat(productQuantityText);
+
+    // get product balance form html tag and convert it number
+    const balanceTotal = document.getElementById('balance-1');
+    const balanceText = balanceTotal.innerText;
+    const newBalance = parseFloat(balanceText);
+
+    // product quantity 
+    const producttotal = newProductQuantity + 1;
+    productQuantityField.innerText = producttotal
+
+    // get product price form html tag and convert it number
+    const productPriceField = document.getElementById('product-price');
+    const productPriceText = productPriceField.innerText;
+    const productPriceAmount = parseFloat(productPriceText);
+
+    // product price
+    const newProuctPrice = productPriceAmount + newBalance ;
+    productPriceField.innerText = newProuctPrice
+    
+});
+
+// button-2
+document.getElementById('buy-now-2').addEventListener('click',function(){
+
+    // get product quantity form html tag and convert it number
+    const productQuantityField = document.getElementById('product-quantity');
+    const productQuantityText = productQuantityField.innerText; 
+    const newProductQuantity = parseFloat(productQuantityText);
+
+    // get product balance form html tag and convert it number
+    const balanceTotal = document.getElementById('balance-2');
+    const balanceText = balanceTotal.innerText;
+    const newBalanc = parseFloat(balanceText);
+
+    // product quantity
+    const producttotal = newProductQuantity + 1;
+    productQuantityField.innerText = producttotal
+
+    // get product price form html tag and convert it number
+    const productPriceField = document.getElementById('product-price');
+    const productPriceText = productPriceField.innerText;
+    const productPriceAmount = parseFloat(productPriceText);
+
+    // product price
+    const newProuctPrice = productPriceAmount + newBalanc ;
+    productPriceField.innerText = newProuctPrice
+});
+
+// button-3
+document.getElementById('buy-now-3').addEventListener('click',function(){
+
+    // get product quantity form html tag and convert it number
+    const productQuantityField = document.getElementById('product-quantity');
+    const productQuantityText = productQuantityField.innerText; 
+    const newProductQuantity = parseFloat(productQuantityText);
+
+    // get product balance form html tag and convert it number
+    const balanceTotal = document.getElementById('balance-3');
+    const balanceText = balanceTotal.innerText;
+    const newBalanc = parseFloat(balanceText);
+
+    // product quantity
+    const producttotal = newProductQuantity + 1;
+    productQuantityField.innerText = producttotal
+
+    // get product price form html tag and convert it number
+    const productPriceField = document.getElementById('product-price');
+    const productPriceText = productPriceField.innerText;
+    const productPriceAmount = parseFloat(productPriceText);
+
+    // product price
+    const newProuctPrice = productPriceAmount + newBalanc ;
+    productPriceField.innerText = newProuctPrice
+});
+
+// buttton-4
+document.getElementById('buy-now-4').addEventListener('click',function(){
+
+    // get product quantity form html tag and convert it number
+    const productQuantityField = document.getElementById('product-quantity');
+    const productQuantityText = productQuantityField.innerText; 
+    const newProductQuantity = parseFloat(productQuantityText);
+
+    // get product balance form html tag and convert it number
+    const balanceTotal = document.getElementById('balance-4');
+    const balanceText = balanceTotal.innerText;
+    const newBalanc = parseFloat(balanceText);
+
+    // product quantity
+    const producttotal = newProductQuantity + 1;
+    productQuantityField.innerText = producttotal
+
+    // get product price form html tag and convert it number
+    const productPriceField = document.getElementById('product-price');
+    const productPriceText = productPriceField.innerText;
+    const productPriceAmount = parseFloat(productPriceText);
+
+    // product price
+    const newProuctPrice = productPriceAmount + newBalanc ;
+    productPriceField.innerText = newProuctPrice
+});
+
+// button-5
+document.getElementById('buy-now-5').addEventListener('click',function(){
+
+    // get product quantity form html tag and convert it number
+    const productQuantityField = document.getElementById('product-quantity');
+    const productQuantityText = productQuantityField.innerText; 
+    const newProductQuantity = parseFloat(productQuantityText);
+
+    // get product balance form html tag and convert it number
+    const balanceTotal = document.getElementById('balance-5');
+    const balanceText = balanceTotal.innerText;
+    const newBalanc = parseFloat(balanceText);
+
+    // product quantity
+    const producttotal = newProductQuantity + 1;
+    productQuantityField.innerText = producttotal
+
+    // get product price form html tag and convert it number
+    const productPriceField = document.getElementById('product-price');
+    const productPriceText = productPriceField.innerText;
+    const productPriceAmount = parseFloat(productPriceText);
+
+    // product price
+    const newProuctPrice = productPriceAmount + newBalanc ;
+    productPriceField.innerText = newProuctPrice
+});
+
+// button-6
+document.getElementById('buy-now-6').addEventListener('click',function(){
+
+    // get product quantity form html tag and convert it number
+    const productQuantityField = document.getElementById('product-quantity');
+    const productQuantityText = productQuantityField.innerText; 
+    const newProductQuantity = parseFloat(productQuantityText);
+
+    // get product balance form html tag and convert it number
+    const balanceTotal = document.getElementById('balance-6');
+    const balanceText = balanceTotal.innerText;
+    const newBalanc = parseFloat(balanceText);
+
+    // product quantity
+    const producttotal = newProductQuantity + 1;
+    productQuantityField.innerText = producttotal
+
+    // get product price form html tag and convert it number
+    const productPriceField = document.getElementById('product-price');
+    const productPriceText = productPriceField.innerText;
+    const productPriceAmount = parseFloat(productPriceText);
+
+    // product price
+    const newProuctPrice = productPriceAmount + newBalanc ;
+    productPriceField.innerText = newProuctPrice
+});
+
+// button-7
+document.getElementById('buy-now-7').addEventListener('click',function(){
+
+    // get product quantity form html tag and convert it number
+    const productQuantityField = document.getElementById('product-quantity');
+    const productQuantityText = productQuantityField.innerText; 
+    const newProductQuantity = parseFloat(productQuantityText);
+
+    // get product balance form html tag and convert it number
+    const balanceTotal = document.getElementById('balance-7');
+    const balanceText = balanceTotal.innerText;
+    const newBalanc = parseFloat(balanceText);
+
+    // product quantity
+    const producttotal = newProductQuantity + 1;
+    productQuantityField.innerText = producttotal
+
+    // get product price form html tag and convert it number
+    const productPriceField = document.getElementById('product-price');
+    const productPriceText = productPriceField.innerText;
+    const productPriceAmount = parseFloat(productPriceText);
+
+    // product price
+    const newProuctPrice = productPriceAmount + newBalanc ;
+    productPriceField.innerText = newProuctPrice
+});
+
+// button-8
+document.getElementById('buy-now-8').addEventListener('click',function(){
+
+    // get product quantity form html tag and convert it number
+    const productQuantityField = document.getElementById('product-quantity');
+    const productQuantityText = productQuantityField.innerText; 
+    const newProductQuantity = parseFloat(productQuantityText);
+
+    // get product balance form html tag and convert it number
+    const balanceTotal = document.getElementById('balance-8');
+    const balanceText = balanceTotal.innerText;
+    const newBalanc = parseFloat(balanceText);
+
+    // product quantity
+    const producttotal = newProductQuantity + 1;
+    productQuantityField.innerText = producttotal
+
+    // get product price form html tag and convert it number
+    const productPriceField = document.getElementById('product-price');
+    const productPriceText = productPriceField.innerText;
+    const productPriceAmount = parseFloat(productPriceText);
+
+    // product price
+    const newProuctPrice = productPriceAmount + newBalanc ;
+    productPriceField.innerText = newProuctPrice
+});
+
+// button-9
+document.getElementById('buy-now-9').addEventListener('click',function(){
+
+    // get product quantity form html tag and convert it number
+    const productQuantityField = document.getElementById('product-quantity');
+    const productQuantityText = productQuantityField.innerText; 
+    const newProductQuantity = parseFloat(productQuantityText);
+
+    // get product balance form html tag and convert it number
+    const balanceTotal = document.getElementById('balance-9');
+    const balanceText = balanceTotal.innerText;
+    const newBalanc = parseFloat(balanceText);
+
+    // product quantity
+    const producttotal = newProductQuantity + 1;
+    productQuantityField.innerText = producttotal
+
+    // get product price form html tag and convert it number
+    const productPriceField = document.getElementById('product-price');
+    const productPriceText = productPriceField.innerText;
+    const productPriceAmount = parseFloat(productPriceText);
+
+    // product price
+    const newProuctPrice = productPriceAmount + newBalanc ;
+    productPriceField.innerText = newProuctPrice
+});
